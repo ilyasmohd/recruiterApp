a12eaab3d5c89df02a8727d6033fbc2be8914a40
ilyasmohd <md.ilyas89@gmail.com>
2020-04-02 14:47:09 +0300

    employer zone changes

--- src/app/employer/employer.component.ts
+++ src/app/employer/employer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployerComponent implements OnInit {
 
+  public optionSelected:string = "";
   constructor() { }
 
   ngOnInit() {
