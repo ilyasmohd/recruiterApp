abe4f1877ef285e5636a980b726af739e4bc3ad2
shahid <shahidtabrez>
2020-08-06 20:15:36 +0530

    new ui changes done

--- src/app/other-services/other-services.component.ts
+++ src/app/other-services/other-services.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-other-services',
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OtherServicesComponent implements OnInit {
 
+  isShow: boolean;
+  topPosToStartShowing = 100;
   constructor() { }
 
   ngOnInit() {
   }
+
+
+  @HostListener('window:scroll')
+  checkScroll() {
+      
+    // window의 scroll top
+    // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. window.pageYOffset is not supported below IE 9.
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    console.log('[scroll]', scrollPosition);
+    
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+  
+  // TODO: Cross browsing
+  gotoTop() {
+    window.scroll({ 
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });
+  }
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({behavior:"smooth"});
+}
 
 }
